eedea53f83cc872232dd0ff36c2f8b032732b6f7
Jibran Ibrahim Syed <jisyed14@gmail.com>
2018-02-21 12:43:41 -0600

    Designing the interface for category selection

---
+++ Chott/Chott/Utilities/AppColors.swift
@@ -0,0 +1,31 @@
+//
+//  AppColors.swift
+//  Chott
+//
+//  Created by Jibran Syed on 2/21/18.
+//  Copyright © 2018 Jishenaz. All rights reserved.
+//
+
+import UIKit    // For UIColor
+
+enum AppColors 
+{
+    static let appBkg = #colorLiteral(red: 0.3333333333, green: 0.3843137255, blue: 0.4392156863, alpha: 1)
+    static let appBkgLite = #colorLiteral(red: 0.5411764706, green: 0.6078431373, blue: 0.6823529412, alpha: 1)
+    
+    static let categoryCodeRegular = #colorLiteral(red: 0.3058823529, green: 0.8039215686, blue: 0.768627451, alpha: 1)
+    static let categoryCodeLite = #colorLiteral(red: 0.7647058824, green: 0.968627451, blue: 0.9098039216, alpha: 1)
+    static let categoryCodeDark = #colorLiteral(red: 0.1098039216, green: 0.6274509804, blue: 0.631372549, alpha: 1)
+    
+    static let categoryArtRegular = #colorLiteral(red: 0.5607843137, green: 0.8509803922, blue: 0.0862745098, alpha: 1)
+    static let categoryArtLite = #colorLiteral(red: 0.8823529412, green: 1, blue: 0.7921568627, alpha: 1)
+    static let categoryArtDark = #colorLiteral(red: 0.4745098039, green: 0.6980392157, blue: 0.1176470588, alpha: 1)
+    
+    static let categoryMusicRegular = #colorLiteral(red: 1, green: 0.4196078431, blue: 0.4196078431, alpha: 1)
+    static let categoryMusicLite = #colorLiteral(red: 1, green: 0.8117647059, blue: 0.831372549, alpha: 1)
+    static let categoryMusicDark = #colorLiteral(red: 0.831372549, green: 0.1764705882, blue: 0.1803921569, alpha: 1)
+    
+    static let categoryWritingRegular = #colorLiteral(red: 0.768627451, green: 0.3254901961, blue: 0.5098039216, alpha: 1)
+    static let categoryWritingLite = #colorLiteral(red: 0.9411764706, green: 0.7764705882, blue: 0.8901960784, alpha: 1)
+    static let categoryWritingDark = #colorLiteral(red: 0.5843137255, green: 0.1490196078, blue: 0.3803921569, alpha: 1)
+}
