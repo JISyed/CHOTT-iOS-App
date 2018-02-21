cc57aca4dc3788e23bf8f94e0dfdbd9416087126
Jibran Ibrahim Syed <jisyed14@gmail.com>
2018-02-21 15:36:38 -0600

    Minor code adjustments

---
+++ Chott/Chott/Controller/CategoryViewController.swift
@@ -0,0 +1,22 @@
+//
+//  CategoryViewController.swift
+//  Chott
+//
+//  Created by Jibran Syed on 2/20/18.
+//  Copyright © 2018 Jishenaz. All rights reserved.
+//
+
+import UIKit
+
+class CategoryViewController: UIViewController 
+{
+
+    override func viewDidLoad() 
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+}
+
