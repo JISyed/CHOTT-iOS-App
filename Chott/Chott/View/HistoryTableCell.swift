b2ba89019fc2266f9a0a52e4409a0d4597eff937
Jibran Ibrahim Syed <jisyed14@gmail.com>
2018-02-28 16:36:39 -0600

    HistoryVC now uses different shades on the duration text depending if a session was seconds long, minutes long, or hours long.

--- Chott/Chott/View/HistoryTableCell.swift
+++ Chott/Chott/View/HistoryTableCell.swift
@@ -22,6 +22,23 @@ class HistoryTableCell: UITableViewCell
         self.lblDate.text = DateUtility.getDateString(for: session.startTime!)
         self.lblStartAndEndTimes.text = DateUtility.getTimeRangeString(from: session.startTime!, to: session.endTime!)
         self.lblDuration.text = DateUtility.getTimeIntervalString(from: session.startTime!, to: session.endTime!)
+        
+        // Change shade of duration text depending on length of time (1 hour+ will be white)
+        if self.lblDuration.text!.contains("min") 
+        {
+            if self.lblDuration.text!.contains("hr") 
+            {
+                self.lblDuration.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+            }
+            else
+            {
+                self.lblDuration.textColor = #colorLiteral(red: 0.8660572652, green: 0.8660572652, blue: 0.8660572652, alpha: 1)
+            }
+        }
+        if self.lblDuration.text!.contains("sec") 
+        {
+            self.lblDuration.textColor = #colorLiteral(red: 0.7021409648, green: 0.7021409648, blue: 0.7021409648, alpha: 1)
+        }
     }
     
     
